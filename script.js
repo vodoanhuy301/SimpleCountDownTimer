193c016c81d016c6db371a760099b1cb042d6540
vodoanhuy301 <vodoanhuy301@gmail.com>
2021-12-28 21:36:06 +0700

    update using jQuery methods

--- script.js
+++ script.js
@@ -1,52 +1,45 @@
-const daysElement = document.getElementById('days');
-const hoursElement = document.getElementById('hours');
-const minutesElement = document.getElementById('minutes');
-const secondsElement = document.getElementById('seconds');
-const musicElement = document.getElementById('musicTrack');
-const titleElement = document.querySelector('h1');
-const clockElement = document.querySelector('.container_clock');
-const settingTimeElement = document.getElementById('timeSet');
+const daysElement = $("#days");
+const hoursElement = $("#hours");
+const minutesElement = $("#minutes");
+const secondsElement = $("#seconds");
+const musicElement = $("#musicTrack");
+const titleElement = $("h1");
+const clockElement = $(".container_clock");
+const settingTimeElement = $("#timeSet");
 
-
-
-document.getElementById("btn-1").addEventListener("click", function(){
-    changeTime();
+$("#btn-1").click(function () {
+  changeTime();
 });
 setInterval(countDown, 1000);
-var setTime = document.getElementById('timeSet').value;
+var setTime = $("#timeSet").val();
 function countDown() {
-    if (setTime){
-        const newDate = new Date(setTime);
-        const currentDate = new Date();
-        const totalSeconds = (newDate - currentDate) /1000;
-        console.log(totalSeconds);
-        if (totalSeconds > 0){
-            const days = Math.floor(totalSeconds/3600/24);
-            const hours = Math.floor(totalSeconds/3600) % 24;
-            const minutes = Math.floor(totalSeconds/60) %60;
-            const seconds = Math.floor(totalSeconds) % 60;
-        
-        
-            daysElement.innerHTML = formatTime(days);
-            hoursElement.innerHTML = formatTime(hours);
-            minutesElement.innerHTML = formatTime(minutes);
-            secondsElement.innerHTML = formatTime(seconds);
-        }   
-        else  
-            {
-            titleElement.innerText = "Yeah!!!";
-            clockElement.style.display = "none";
-            playMusic();
-            }
+  if (setTime) {
+    const newDate = new Date(setTime);
+    const currentDate = new Date();
+    const totalSeconds = (newDate - currentDate) / 1000;
+    if (totalSeconds > 0) {
+      const days = Math.floor(totalSeconds / 3600 / 24);
+      const hours = Math.floor(totalSeconds / 3600) % 24;
+      const minutes = Math.floor(totalSeconds / 60) % 60;
+      const seconds = Math.floor(totalSeconds) % 60;
+
+      daysElement.html(formatTime(days));
+      hoursElement.html(formatTime(hours));
+      minutesElement.html(formatTime(minutes));
+      secondsElement.html(formatTime(seconds));
+    } else {
+      titleElement.text("Yeah!!!");
+      clockElement.css("display", "none");
+      playMusic();
     }
-    
+  }
 }
 function playMusic() {
-    musicElement.play();
+  musicElement.play();
 }
-function formatTime(time){
-    return time < 10 ? `0${time}` : time;
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 }
-function changeTime(){
-    setTime = document.getElementById('timeSet').value;
+function changeTime() {
+  setTime = $("#timeSet").val();
 }
